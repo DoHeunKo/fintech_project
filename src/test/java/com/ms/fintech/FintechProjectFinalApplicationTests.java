5d84500dd9ccab354a4d1bad78be1ce1ddca4c47
doheun <ehgms9332@gmail.com>
2023-06-13 15:47:51 +0900

    setting

---
+++ src/test/java/com/ms/fintech/FintechProjectFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechProjectFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
