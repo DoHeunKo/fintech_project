249ca9e49f5c624596ce3346774cf6f930dde22b
doheun <ehgms9332@gmail.com>
2023-06-13 17:28:33 +0900

    layout설정

---
+++ src/main/java/com/ms/fintech/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.ms.fintech;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.cors().disable()   
+			.csrf().disable()
+			.formLogin().disable()  //시큐리티 기본 로그인 폼 사용 안함
+		    .headers().frameOptions().disable();
+		
+		return http.build();
+	}
+	
+//	패스워드 암호화 해줄 객체
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+}
