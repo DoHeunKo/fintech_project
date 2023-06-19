9927a1a8dd2c2401d96c71a6d8d67cdb7aac987a
doheun <ehgms9332@gmail.com>
2023-06-19 17:44:21 +0900

    패턴분석 구현중

--- src/main/java/com/ms/fintech/controller/UserController.java
+++ src/main/java/com/ms/fintech/controller/UserController.java
@@ -2,23 +2,31 @@ package com.ms.fintech.controller;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ms.fintech.apidtos.UserMeDto;
 import com.ms.fintech.command.LoginCommand;
 import com.ms.fintech.dtos.RoomDto;
 import com.ms.fintech.dtos.UserDto;
+import com.ms.fintech.feign.AccountFeign;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+	@Autowired
+	private AccountFeign accountFeign;
+	
 	@GetMapping("/userMain")
 	public String userMain() {
 		return "thymeleaf/user/userMain";
@@ -55,4 +63,32 @@ public class UserController {
 		}
 		return "thymeleaf/chatroom";
 	}
+	
+	@GetMapping("/analysis")
+	public String analysis() {
+		return "thymeleaf/user/analysis";
+	}
+
+	@ResponseBody
+	@GetMapping("/allmyinfo")
+	public String myInfo(HttpServletRequest request){
+//		public UserMeDto myInfo(HttpServletRequest request){
+			System.out.println("나의 정보조회하기");
+			
+			HttpSession session=request.getSession();
+			UserDto dto=(UserDto)session.getAttribute("dto");
+			System.out.println(dto);
+			
+			//feign 인터페이스가 요청해주고 결과값도 받아준다
+			UserMeDto userMeDto= accountFeign
+					.requestUserMe("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIxMTAxMDMyODM4Iiwic2NvcGUiOlsiaW5xdWlyeSIsImxvZ2luIiwidHJhbnNmZXIiXSwiaXNzIjoiaHR0cHM6Ly93d3cub3BlbmJhbmtpbmcub3Iua3IiLCJleHAiOjE2OTQ5MTU5MDQsImp0aSI6IjdmMjMwMWQzLWY2MzAtNDVkMi05MDBmLTYzNjVjYTU2MDMyZiJ9.Qn0KErtxaESGeyrcbVvua35VfFNk1K-3aSUJUwhY9Yc", dto.getUser_seq_no()+"");
+			
+//			System.out.println(userMeDto.getUser_name());
+			//결과값을 받아서 request 스코프에 담아서 전달할 수 있다.
+			System.out.println(userMeDto);
+			request.setAttribute("userMeDto", userMeDto);
+			
+//			return userMeDto;	
+			return "thymeleaf/user/analysis";
+		}
 }
