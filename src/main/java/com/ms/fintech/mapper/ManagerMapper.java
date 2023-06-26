bc4ba9e8aec65afe0887b6267c6f5f23d39e73a5
= <=>
2023-06-26 09:38:21 +0900

    manager page 구현

--- src/main/java/com/ms/fintech/mapper/ManagerMapper.java
+++ src/main/java/com/ms/fintech/mapper/ManagerMapper.java
@@ -1,7 +1,16 @@
 package com.ms.fintech.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.ms.fintech.dtos.CrawlerDto;
+import com.ms.fintech.dtos.UserDto;
 
 @Mapper
 public interface ManagerMapper {
+	
+	@Select("select * from mz_info limit #{page}, 6")
+	public List<CrawlerDto> getPagedList(int page);
 }
