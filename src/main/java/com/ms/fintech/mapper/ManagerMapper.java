a39054ccae26646aea3111b77d614048dd6ef645
= <=>
2023-06-27 13:02:36 +0900

    manager 관련 페이지 수정

--- src/main/java/com/ms/fintech/mapper/ManagerMapper.java
+++ src/main/java/com/ms/fintech/mapper/ManagerMapper.java
@@ -7,17 +7,22 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import com.ms.fintech.dtos.CrawlerDto;
+import com.ms.fintech.dtos.RoomDto;
 
 @Mapper
 public interface ManagerMapper {
-	
+
 	@Select("select * from mz_info limit #{page}, 6")
 	public List<CrawlerDto> getPagedList(int page);
 	
+	@Select("select * from community_info limit #{page}, 4")
+	public List<RoomDto> getPagedRoom(int page);
+	
+	@Delete("delete from community_info where room_no = #{seq}")
+	public int deleteRoom(int seq);
 
 	@Delete("delete from mz_info where seq = #{seq}")
 	public int deleteNews(int seq);
-
-	@Delete("delete from mz_info where seq = #{seq}")
-	public int deleteRoom(int seq);
+	
+	
 }
